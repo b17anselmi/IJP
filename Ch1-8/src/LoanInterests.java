cc2ac94174ac41283d51704d378f7c103cf89380
b17anselmi <32404050+b17anselmi@users.noreply.github.com>
2017-12-31 17:34:01 -0700

    Exercise 5.19 and 5.21, powers of three pyramid and loan calculator

---
+++ Ch1-8/src/LoanInterests.java
@@ -0,0 +1,29 @@
+/* Exercise 5.21 given a loan amount and period, display monthly and total payments for each interest rate 
+ * between 5%-10% in 1/4 steps (i.e. 5.0, 5.25, 5.5, etc)
+ * 
+ */
+import java.util.Scanner;
+public class LoanInterests {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Loan Amount: ");
+		double loan = s.nextDouble();//get loan amount
+		System.out.println("Period in years: ");
+		double period = s.nextDouble();//get number of years of the loan period
+		s.close();//close scanner
+		double interest = 5.0; //initiate interest at 5%
+		System.out.printf("%-1s%20s%20s\n", "Interest Rate", "Monthly Payment","Total Payment");//print header
+		do {
+			double monthlyrate, monthly, total;
+			monthlyrate = interest/1200;
+			monthly = loan*monthlyrate/(1 - Math.pow(1 + monthlyrate, -(period * 12)));
+			total = monthly*period*12;
+			
+			System.out.println(interest+ "    "+ monthly + "    "+ total ); 
+			interest += 0.25;
+		}while (interest<=10.0);//end computation once the interest rate is 10%
+
+	}
+
+}
---
+++ Ch1-8/src/powersof3pyramid.java
@@ -0,0 +1,34 @@
+/*display a pyramid
+ * 			1
+ * 		1	3	1
+ * 1		3	9	3	1
+ * and so on until 2187 is bottom middle digit
+ */
+
+public class powersof3pyramid {
+
+	public static void main(String[] args) {
+		  //8 leveles of the pyramid
+		  int n = 8;
+		 
+		  for (int i = 1; i <= n; i++) {
+		   // printing spaces
+		   for (int j = 1; j <= (n - i); j++) {
+		    System.out.print("    ");
+		   }
+		 
+		   // Printing number incrementally from 0 to i-1
+		   for (int j = 0; j < i; j++) {
+		    System.out.printf("%4d", (int) Math.pow(3, j));
+		   }
+		 
+		   // Printing number decreamentally from i-2 to 0
+		   for (int j = i - 2; j >= 0; j--) {
+		    System.out.printf("%4d", (int) Math.pow(3, j));
+		   }
+		   System.out.println(); //got to next line
+
+	}
+
+}
+}

95307aecc205326fafe69fef623cd9933505f0e6
b17anselmi <32404050+b17anselmi@users.noreply.github.com>
2018-01-03 19:44:13 -0700

    Exercise 6.26 print first 120 palindromic prime numbers.

---
+++ Ch1-8/src/PalindromePrimes.java
@@ -0,0 +1,27 @@
+// 6.26 display first 120 numbers that are prime and palindromic 
+public class PalindromePrimes {
+
+	public static void main(String[] args) {
+		
+		int c =0,i =0;
+		do{
+		if (PalidromeInt.isPalindrome(i)&& Prime(i)) {//I already created a method to check if a number is palindromic or not, 
+			System.out.printf("%10d", i);
+            c++;
+            if (c % 10 == 0 && i != 0); 
+		}
+		i++;
+		}while(c<=120);
+	}
+
+	public static boolean Prime(long n) {
+		if (n<2) return false;
+		for (int i = 2; i <= n / 2; i++) {
+
+            if (n % i == 0) return false;
+        }
+		
+		return true;
+	}
+
+}
